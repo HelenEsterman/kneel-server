238ed1c64617d034ab6b7e19c7a5db6b0411ae7f
Helen Esterman <helenesterman99@gmail.com>
2023-10-31 16:11:47 -0400

    added get functionality for orders

--- views/OrderView.py
+++ views/OrderView.py
@@ -1,3 +1,33 @@
+import json
+from sqlFetch import db_get_single, db_get_all
+from nss_handler import status
+
+
 class OrderView:
     def get(self, handler, pk):
-        pass
+        if pk != 0:
+            sql = """
+                SELECT 
+                o.id,
+                o.metalId,
+                o.sizeId,
+                o.styleId
+                FROM `Orders` o
+                WHERE o.id = ?
+                """
+            query_results = db_get_single(sql, pk)
+            order_json_object = json.dumps(dict(query_results))
+            return handler.response(order_json_object, status.HTTP_200_SUCCESS.value)
+        else:
+            sql = """
+                SELECT 
+                o.id,
+                o.metalId,
+                o.sizeId,
+                o.styleId
+                FROM `Orders` o
+                """
+            query_results = db_get_all(sql)
+            orders = [dict(row) for row in query_results]
+            orders_json_array = json.dumps(orders)
+            return handler.response(orders_json_array, status.HTTP_200_SUCCESS.value)
